35deac3bf1cb44da971e8ddba3acb3facec95733
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-18 01:36:50 -0300

    I put the check icon and close icon

--- src/components/header/ListStructure.jsx
+++ src/components/header/ListStructure.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../styles/headerStyle/liststructure.css'
 import { FaCheck } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 const ListStructure = () => {
 
     const listProducts = ['Tester', 'Just test']
@@ -9,7 +10,7 @@ const ListStructure = () => {
     return (
     <ul id='ul_list'>
         {listProducts.map((item) => (
-            <li>{item} <FaCheck size={21} id='check_icon' /></li>
+            <li>{item} <FaCheck size={21} id='check_icon' /> <IoClose id='close_item_list_icon' size={30} /></li>
         ))} 
     </ul>
   )
