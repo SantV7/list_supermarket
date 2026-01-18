ce5f3fd04734cc38dfd4e57bee67a8a51b54a4f6
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-18 00:44:49 -0300

    trying to make menu and subcategory

--- src/components/header/HeaderPage.jsx
+++ src/components/header/HeaderPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import { FaCartShopping } from "react-icons/fa6";
+import { MdAddShoppingCart } from "react-icons/md";
 import { MdRemoveShoppingCart } from "react-icons/md";
+import { CiSearch } from "react-icons/ci";
 import '../../styles/headerStyle/headerpage.css'
 
 const HeaderPage = () => {
@@ -13,13 +14,40 @@ const HeaderPage = () => {
       <>
       <header id='header'>
 
-        <div className="searchItens">
-           <input type="search" placeholder='Pesquisar item'  id="searchSearch" />
+        <div className="searchItens" id="searchSearch">
+           <div className="area_search">
+             <input type="search" placeholder='Pesquisar item'   />
+             <CiSearch size={30} id='search_icon' />
+           </div>
 
            <div onClick={() => SetStatusBtnCart(!statusBtnCart)} className="area_cart">
              {statusBtnCart
-               ? <FaCartShopping size={35} />
-               : <MdRemoveShoppingCart size={35} />
+               ? <MdAddShoppingCart size={33} />
+               : <MdRemoveShoppingCart size={33} />
+             }
+
+             {
+              statusBtnCart
+              ? (
+                 <div className="menuList">
+                  <section id='basic_section'>
+                    <h2 className='tittle_category'>Básicos</h2>
+                  </section>
+
+                  <section id='basic_section'>
+                    <h2 className='tittle_category'>Lanches</h2>
+                  </section>
+
+                  <section id='fruits_and_vegetables_section'>
+                    <h2 className='tittle_category'>frutas e legumes</h2>
+                  </section>
+
+                  <section id='bebidas_section'>
+                    <h2 className='tittle_category'>Bebidas</h2>
+                  </section>
+                 </div>
+              )
+              : ('')
              }
            </div>
         </div>
