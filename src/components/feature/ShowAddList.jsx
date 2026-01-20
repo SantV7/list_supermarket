f954d0344a33926663c997a613ab4ae999ca7558
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-20 00:35:01 -0300

    Finished style on mobile version

--- src/components/feature/ShowAddList.jsx
+++ src/components/feature/ShowAddList.jsx
@@ -1,5 +1,5 @@
 import '../../styles/showaddlist.css/showaddlist.css'
-
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const ShowAddList = () => {
 
@@ -11,14 +11,16 @@ const ShowAddList = () => {
         <h2>Adicionar Item</h2>
 
         <div id='category_area_input' className="input-add-list">
-            <label htmlFor="category_input">Categoria:</label>
-            <input type="text" id='category_input' placeholder="Adicionar categoria" />       
+            <label htmlFor="category_input">Categoria<FaLongArrowAltRight size={25}  className='arrow-to-input'/></label>
+            <input type="text" id='category_input' placeholder="Adicionar" />       
         </div>
 
         <div id='input_area_input' className="input-add-list">
-          <label htmlFor="item_input">Item:</label>
-          <input type="text" id='item_input' placeholder='Adicionar item' />
+          <label htmlFor="item_input">Item <FaLongArrowAltRight  size={25} className='arrow-to-input'/></label>
+          <input type="text" id='item_input' placeholder='Adicionar' />
         </div>
+
+        <button id='add_adn_save_on_list'>Adicionar a lista</button>
 
       </div>
     </>
